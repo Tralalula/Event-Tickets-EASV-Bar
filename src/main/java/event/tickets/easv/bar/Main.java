a2d61003000a239b196df19b74e85cf76399e4b1
Tobias <qenzoz@gmail.com>
2024-03-12 16:11:34 +0100

    Opsæt projekt

---
+++ src/main/java/event/tickets/easv/bar/Main.java
@@ -0,0 +1,15 @@
+package event.tickets.easv.bar;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        Application.launch();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
