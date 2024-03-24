c89702457d48846a7e7bd7cf8c1969014a296721
Tobias <qenzoz@gmail.com>
2024-03-24 21:13:46 +0100

    Opsæt DAO interface

---
+++ src/main/java/event/tickets/easv/bar/dal/database/common/DAO.java
@@ -0,0 +1,54 @@
+package event.tickets.easv.bar.dal.database.common;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * DAO (Data Access Object) represents a generic interface for performing CRUD (Create, Read, Update, Delete)
+ * operations on a collection of entities of type T.<br>
+ * <br>
+ * All methods throw an Exception if a problem occurs during data access.
+ *
+ * @param <T> the type of the entity that the DAO will manage.
+ */
+public interface DAO<T> {
+    /**
+     * Retrieves an entity by its identifier.
+     *
+     * @param id the unique identifier of the entity to retrieve.
+     * @return an optional containing the retrieved entity; otherwise an empty Optional.
+     */
+    Optional<T> get(int id) throws Exception;
+
+    /**
+     * Retrieves all entities of type T.
+     *
+     * @return a list containing all entities of type T found; the list can be empty.
+     */
+    List<T> getAll() throws Exception;
+
+    /**
+     * Adds a new entity to the data source.
+     *
+     * @param t the entity to add.
+     * @return the added entity.
+     */
+    T add(T t) throws Exception;
+
+    /**
+     * Updates an existing entity with new data.
+     *
+     * @param original the original entity to update.
+     * @param updatedData the entity containing the updated data.
+     * @return true if the update was successful; false otherwise.
+     */
+    boolean update(T original, T updatedData) throws Exception;
+
+    /**
+     * Deletes an entity from the data source.
+     *
+     * @param t the entity to delete.
+     * @return true if the deletion was successful; false otherwise.
+     */
+    boolean delete(T t) throws Exception;
+}
