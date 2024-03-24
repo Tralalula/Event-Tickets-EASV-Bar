63c53c61473a3570c6bb5788ba79a499f45e64e2
Tobias <qenzoz@gmail.com>
2024-03-24 21:22:18 +0100

    Tilføj script.sql med Events test data

---
+++ src/main/java/event/tickets/easv/bar/dal/database/script/script.sql
@@ -0,0 +1,36 @@
+USE EventManager;
+GO
+
+IF OBJECT_ID('dbo.Events', 'U') IS NOT NULL
+    DROP TABLE dbo.Events;
+GO
+
+CREATE TABLE Events (
+                        id INT PRIMARY KEY IDENTITY(1,1),
+                        title NVARCHAR(255) NOT NULL
+);
+GO
+
+INSERT INTO Events (title)
+VALUES
+    ('International Food Festival'),
+    ('Vegan Cooking Workshop'),
+    ('Farm to Table Dinner'),
+    ('Wine and Cheese Night'),
+    ('Italian Pasta Making Class'),
+    ('French Cuisine Tasting'),
+    ('Sushi Rolling Workshop'),
+    ('Chocolate Making Class'),
+    ('BBQ and Grill Cook-off'),
+    ('Farmers Market Tour'),
+    ('Pastry and Baking Workshop'),
+    ('Coffee Tasting Experience'),
+    ('Beer Brewing Demonstration'),
+    ('Gourmet Burger Festival'),
+    ('Mexican Fiesta Night'),
+    ('Street Food Extravaganza'),
+    ('Ice Cream Social'),
+    ('Pizza Making Party'),
+    ('Seafood Feast'),
+    ('Culinary Arts Festival');
+GO
