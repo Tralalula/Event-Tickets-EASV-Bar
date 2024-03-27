b3873a6ba6f424782648d7842d615de542791f1a
Tobias <qenzoz@gmail.com>
2024-03-27 22:54:50 +0100

    Tilføj profilbilleder til event card

--- src/main/java/event/tickets/easv/bar/gui/component/events/EventsView.java
+++ src/main/java/event/tickets/easv/bar/gui/component/events/EventsView.java
@@ -16,12 +16,15 @@ import javafx.beans.binding.StringBinding;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.ObjectProperty;
 import javafx.collections.ObservableList;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressIndicator;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
 import org.controlsfx.control.GridCell;
 import org.controlsfx.control.GridView;
 
@@ -35,6 +38,12 @@ import java.util.Locale;
 import java.util.Map;
 
 public class EventsView implements View {
+    private static final int CARD_WIDTH = 326;
+    private static final int CARD_HEIGHT = 370;
+    private static final int PROFILE_IMG_RADIUS = 20;
+    private static final int CARRD_EVENT_IMAGE_WIDTH = CARD_WIDTH - 2; // needs to account for a bit on each side
+    private static final int CARD_EVENT_IMAGE_HEIGHT = 160;
+
     private final ObservableList<EventModel> model;
     private final BooleanProperty fetchingData;
 
@@ -50,8 +59,8 @@ public class EventsView implements View {
 
         var gridview = new GridView<EventModel>();
         gridview.setItems(model);
-        gridview.setCellWidth(326);
-        gridview.setCellHeight(370);
+        gridview.setCellWidth(CARD_WIDTH);
+        gridview.setCellHeight(CARD_HEIGHT);
         gridview.setCellFactory(cell -> eventCell());
 
         return new StackPane(gridview, progressIndicator);
@@ -69,6 +78,14 @@ public class EventsView implements View {
             private final Label endDateTime = new Label();
             private final VBox content = new VBox(title, location, startDateTime, endDateTime);
 
+            private final Circle circle1 = new Circle(PROFILE_IMG_RADIUS);
+            private final Circle circle2 = new Circle(PROFILE_IMG_RADIUS);
+            private final Circle circle3 = new Circle(PROFILE_IMG_RADIUS);
+            private final HBox profileImages = new HBox(StyleConfig.STANDARD_SPACING, circle1, circle2, circle3);
+
+            private final Label ticketsSold = new Label("283 tickets sold");
+
+            private final BorderPane footer = new BorderPane();
 
             {
                 title.getStyleClass().add(Styles.TITLE_3);
@@ -78,21 +95,29 @@ public class EventsView implements View {
                 card.getStyleClass().add(StyleConfig.EVENT_CARD);
                 card.getStyleClass().add(Styles.ELEVATED_4);
 
-                card.setMinWidth(326);
-                card.setMinHeight(370);
-                card.setMaxWidth(326);
-                card.setMaxHeight(370);
-                card.setPrefHeight(370);
-                card.setPrefWidth(326);
+                card.setMinWidth(CARD_WIDTH);
+                card.setMinHeight(CARD_HEIGHT);
 
-                imageView = Images.topRoundImage(324, 160, 5);
-                imageView.setFitWidth(324);
-                imageView.setFitHeight(160);
+                card.setMaxWidth(CARD_WIDTH);
+                card.setMaxHeight(CARD_HEIGHT);
+
+                card.setPrefWidth(CARD_WIDTH);
+                card.setPrefHeight(CARD_HEIGHT);
+
+                imageView = Images.topRoundImage(CARRD_EVENT_IMAGE_WIDTH, CARD_EVENT_IMAGE_HEIGHT, 5);
+                imageView.setFitWidth(CARRD_EVENT_IMAGE_WIDTH);
+                imageView.setFitHeight(CARD_EVENT_IMAGE_HEIGHT);
+
+                ticketsSold.getStyleClass().addAll(Styles.TEXT_SUBTLE, Styles.TEXT_BOLD);
+
+                footer.setLeft(profileImages);
+                footer.setRight(new StackPane(ticketsSold));
+                StackPane.setAlignment(ticketsSold, Pos.BOTTOM_RIGHT);
 
 
                 card.setSubHeader(imageView);
                 card.setBody(content);
-                card.setFooter(new Label("283 tickets sold"));
+                card.setFooter(footer);
             }
 
             @Override
@@ -106,6 +131,10 @@ public class EventsView implements View {
 
                     imageView.setImage(img);
 
+                    loadImagePattern(circle1, getProfileImage("profile1.jpg"));
+                    loadImagePattern(circle2, getProfileImage("profile4.jpg"));
+                    loadImagePattern(circle3, getProfileImage("profile8.jpeg"));
+
                     title.textProperty().bind(item.title());
                     location.textProperty().bind(item.location());
                     startDateTime.textProperty().bind(dateTimeBinding(item.startDate(), item.startTime(), "Starts", formatter));
@@ -118,6 +147,7 @@ public class EventsView implements View {
     }
 
     private static final Map<String, Image> cache = new HashMap<>();
+    private static final Map<String, Image> profileCache = new HashMap<>();
 
     public static Image getImage(String imagePath) {
         Image img = cache.get(imagePath);
@@ -128,6 +158,28 @@ public class EventsView implements View {
         }
 
         return img;
+    }
+
+    public static Image getProfileImage(String imagePath) {
+        Image img = profileCache.get(imagePath);
+        if (img == null) {
+            String absolutePath = "file:///" + Paths.get("").toAbsolutePath() + "/data/profile_images/" + imagePath.replace("\\", "/");
+            img = new Image(absolutePath, true);
+            profileCache.put(imagePath, img);
+        }
+
+        return img;
+    }
+
+    private static void loadImagePattern(Circle circle, Image image) {
+        if (image.getProgress() == 1.0) {
+            circle.setFill(new ImagePattern(image));
+            return;
+        }
+
+        image.progressProperty().addListener((obs, ov, nv) -> {
+            if (nv.doubleValue() == 1.0) circle.setFill(new ImagePattern(image));
+        });
     }
 
     public static StringBinding dateTimeBinding(ObjectProperty<LocalDate> dateProperty,
