edfe61665b90fd8345d192b019558815e5bb4f68
Tobias <qenzoz@gmail.com>
2024-03-28 11:18:21 +0100

    Tilføj doc til WindowType enum

--- src/main/java/event/tickets/easv/bar/gui/common/WindowType.java
+++ src/main/java/event/tickets/easv/bar/gui/common/WindowType.java
@@ -1,6 +1,11 @@
 package event.tickets.easv.bar.gui.common;
 
+/**
+ * Each WindowType is a distinct context or functional area, such as authentication or the main application interface,
+ * which allows for the organization and association of ViewType instances.
+ */
 public enum WindowType {
+    None,
     MAIN_APP,
     AUTH
 }
