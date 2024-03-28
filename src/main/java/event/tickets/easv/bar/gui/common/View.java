657ed77ec8a557b392587722b819ea9dff8781ce
Tobias <qenzoz@gmail.com>
2024-03-28 11:27:46 +0100

    Tilføj specs til View interface

--- src/main/java/event/tickets/easv/bar/gui/common/View.java
+++ src/main/java/event/tickets/easv/bar/gui/common/View.java
@@ -2,6 +2,14 @@ package event.tickets.easv.bar.gui.common;
 
 import javafx.scene.layout.Region;
 
+/**
+ * Represents the View in a Model-View-Controller architecture.
+ */
 public interface View {
+    /**
+     * Retrieves the visual representation of the view.
+     *
+     * @return a Region object that represents the graphical content of the view. This should never be null.
+     */
     Region getView();
 }
