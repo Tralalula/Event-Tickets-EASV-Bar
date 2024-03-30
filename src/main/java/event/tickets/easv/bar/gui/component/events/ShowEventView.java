96901ca570c7e9881b8523372cd14eb83d2008f3
Tobias <qenzoz@gmail.com>
2024-03-30 21:50:49 +0100

    Få billede på show event view

--- src/main/java/event/tickets/easv/bar/gui/component/events/ShowEventView.java
+++ src/main/java/event/tickets/easv/bar/gui/component/events/ShowEventView.java
@@ -8,10 +8,18 @@ import event.tickets.easv.bar.gui.common.ViewHandler;
 import event.tickets.easv.bar.gui.common.ViewType;
 import event.tickets.easv.bar.gui.util.NodeUtils;
 import event.tickets.easv.bar.gui.util.StyleConfig;
+import event.tickets.easv.bar.gui.widgets.Images;
 import event.tickets.easv.bar.gui.widgets.Labels;
+import javafx.application.Platform;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.PixelReader;
+import javafx.scene.image.WritableImage;
 import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 
 import java.time.format.DateTimeFormatter;
@@ -20,24 +28,41 @@ import java.util.Locale;
 public class ShowEventView implements View {
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEEE dd. MMMM HHmm", Locale.ENGLISH);
     private final EventModel model = EventModel.Empty();
+    private final ImageView image;
+
 
     public ShowEventView() {
+        image = new ImageView();
+        image.setPreserveRatio(true);
+        image.setPickOnBounds(true);
+        image.setFitWidth(1200);
+        image.setFitHeight(300);
+
+
         ViewHandler.currentViewDataProperty().subscribe((oldData, newData) -> {
             if (newData instanceof EventModel) {
                 model.update((EventModel) newData);
+                Image img = EventsView.getImage(model.imageName().get());
+                PixelReader reader = img.getPixelReader();
+                WritableImage newImage = new WritableImage(reader, 0, 0, (int) img.getWidth(), 300);
+                image.setImage(newImage);
             }
         });
     }
 
+
     @Override
     public Region getView() {
         var results = new VBox(StyleConfig.STANDARD_SPACING * 4);
+
+        results.setFillWidth(true);
+
         var title = Labels.styledLabel(model.title(), Styles.TITLE_1);
         var location = Labels.styledLabel(model.location(), Styles.TITLE_4);
         var startDateTime = Labels.styledLabel(EventsView.dateTimeBinding(model.startDate(), model.startTime(), "Starts", formatter));
         var endDateTime = Labels.styledLabel(EventsView.dateTimeBinding(model.endDate(), model.endTime(), "Ends", formatter));
 
-        var headerBox = new VBox(title, location, startDateTime, endDateTime);
+        var headerBox = new VBox(image, title, location, startDateTime, endDateTime);
 
         var locationGuidanceText = Labels.styledLabel("Location guidance", Styles.TEXT_BOLD);
         var locationGuidance = Labels.styledLabel(model.locationGuidance());
@@ -48,6 +73,7 @@ public class ShowEventView implements View {
         var noteBox = new VBox(noteText, note);
 
         NodeUtils.bindVisibility(locationGuidanceBox, model.locationGuidance().isNotEmpty());
+        NodeUtils.bindVisibility(noteBox, model.extraInfo().isNotEmpty());
 
         results.getChildren().addAll(headerBox, locationGuidanceBox, noteBox);
 
