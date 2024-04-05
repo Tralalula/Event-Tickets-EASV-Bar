bf066d1f36ce082c67c54a3026c1c43b5099b8e3
Tobias <qenzoz@gmail.com>
2024-04-05 23:14:26 +0200

    Vis navn/rang/mail/telefon/lokation i show user

--- src/main/java/event/tickets/easv/bar/gui/component/users/ShowUserView.java
+++ src/main/java/event/tickets/easv/bar/gui/component/users/ShowUserView.java
@@ -1,23 +1,134 @@
 package event.tickets.easv.bar.gui.component.users;
 
+import atlantafx.base.controls.Spacer;
+import atlantafx.base.theme.Styles;
 import event.tickets.easv.bar.gui.common.EventModel;
 import event.tickets.easv.bar.gui.common.UserModel;
 import event.tickets.easv.bar.gui.common.View;
+import event.tickets.easv.bar.gui.common.ViewHandler;
+import event.tickets.easv.bar.gui.component.events.EventsView;
+import event.tickets.easv.bar.gui.util.NodeUtils;
+import event.tickets.easv.bar.gui.util.StyleConfig;
+import event.tickets.easv.bar.gui.widgets.CircularImageView;
+import javafx.beans.binding.Bindings;
 import javafx.collections.ObservableList;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import org.kordamp.ikonli.feather.Feather;
+import org.kordamp.ikonli.javafx.FontIcon;
+import org.kordamp.ikonli.material2.Material2OutlinedAL;
 
 public class ShowUserView implements View {
     private final UserModel model;
     private final ObservableList<EventModel> masterEventList;
+    private final CircularImageView circularImageView = new CircularImageView(80);
 
     public ShowUserView(UserModel model, ObservableList<EventModel> masterEventList) {
         this.model = model;
         this.masterEventList = masterEventList;
+
+        ViewHandler.currentViewDataProperty().subscribe((oldData, newData) -> {
+            if (newData instanceof UserModel) {
+                model.update((UserModel) newData);
+                circularImageView.setImage(EventsView.getProfileImage(model.id().get() + "/" + model.imageName().get()));
+
+                String firstName = model.firstName().get();
+                String lastName = model.lastName().get();
+                String initials;
+                if (lastName == null || lastName.isEmpty()) {
+                    initials = firstName.length() > 1 ? firstName.substring(0, 2) : firstName;
+                } else {
+                    initials = firstName.substring(0, 1) + lastName.substring(0, 1);
+                }
+
+                circularImageView.setText(initials.toUpperCase());
+            }
+        });
     }
 
     @Override
     public Region getView() {
-        return new Button("Show user");
+        var results = new VBox(StyleConfig.STANDARD_SPACING);
+        results.getStyleClass().addAll(StyleConfig.PADDING_DEFAULT);
+
+        results.getChildren().add(userDataContainer());
+
+        return results;
+    }
+
+    public Node userDataContainer() {
+        var results = new HBox(StyleConfig.STANDARD_SPACING);
+        results.getStyleClass().addAll(Styles.BG_DEFAULT, StyleConfig.PADDING_DEFAULT, StyleConfig.ROUNDING_DEFAULT);
+
+        results.getChildren().addAll(circularImageView.get(), userInfoBox(), new Spacer(), actionIcons());
+
+        return results;
+    }
+
+    private Node actionIcons() {
+        var results = new HBox(StyleConfig.STANDARD_SPACING);
+
+        var editBtn = new Button(null, new FontIcon(Material2OutlinedAL.EDIT));
+        editBtn.getStyleClass().addAll(Styles.BUTTON_CIRCLE, StyleConfig.ACTIONABLE, Styles.FLAT);
+
+        var deleteBtn = new Button(null, new FontIcon(Material2OutlinedAL.DELETE));
+        deleteBtn.getStyleClass().addAll(Styles.BUTTON_CIRCLE, StyleConfig.ACTIONABLE, Styles.FLAT, Styles.DANGER);
+
+        results.getChildren().addAll(editBtn, deleteBtn);
+
+        return results;
+    }
+
+    public Node userInfoBox() {
+        var results = new VBox(StyleConfig.STANDARD_SPACING * 2);
+
+        var name = new Label();
+        var rank = new Label();
+        var email = new Label();
+        var phone = new Label();
+        var location = new Label();
+
+        var nameRank = new VBox((double) StyleConfig.STANDARD_SPACING / 2, name, rank);
+        var info = new VBox(StyleConfig.STANDARD_SPACING, email, phone, location);
+
+        name.textProperty().bind(model.firstName().concat(" ").concat(model.lastName()));
+        rank.textProperty().bind(Bindings.createObjectBinding(
+                () -> switch (model.rank().get()) {
+                    case null -> "";
+                    case ADMIN -> "Admin";
+                    case EVENT_COORDINATOR -> "Event coordinator";
+                }, model.rank()));
+
+        email.textProperty().bind(model.mail());
+        phone.textProperty().bind(model.phoneNumber());
+        location.textProperty().bind(model.location());
+
+        name.getStyleClass().addAll(Styles.TEXT_BOLD, Styles.TITLE_4);
+        rank.getStyleClass().addAll(Styles.TEXT_NORMAL, Styles.TEXT_MUTED);
+        email.getStyleClass().addAll(Styles.TEXT_NORMAL, Styles.TEXT_MUTED);
+        phone.getStyleClass().addAll(Styles.TEXT_NORMAL, Styles.TEXT_MUTED);
+        location.getStyleClass().addAll(Styles.TEXT_NORMAL, Styles.TEXT_MUTED);
+
+        var emailIcon = new FontIcon(Feather.MAIL);
+        var phoneIcon = new FontIcon(Feather.PHONE);
+        var locationIcon = new FontIcon(Feather.MAP_PIN);
+
+        email.setGraphic(emailIcon);
+        phone.setGraphic(phoneIcon);
+        location.setGraphic(locationIcon);
+
+        NodeUtils.bindVisibility(email, model.mail().isNotEmpty());
+        NodeUtils.bindVisibility(phone, model.phoneNumber().isNotEmpty());
+        NodeUtils.bindVisibility(location, model.location().isNotEmpty());
+
+        results.getChildren().addAll(nameRank, info);
+
+        return results;
     }
 }
