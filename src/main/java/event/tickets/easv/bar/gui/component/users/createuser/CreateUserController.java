2ae2b45727a1d2c643359a5632273c8275498c41
Tobias <qenzoz@gmail.com>
2024-04-06 23:25:37 +0200

    Kan sende nulstillet adgangskode på mail

--- src/main/java/event/tickets/easv/bar/gui/component/users/createuser/CreateUserController.java
+++ src/main/java/event/tickets/easv/bar/gui/component/users/createuser/CreateUserController.java
@@ -1,7 +1,9 @@
 package event.tickets.easv.bar.gui.component.users.createuser;
 
+import com.resend.core.exception.ResendException;
 import event.tickets.easv.bar.be.User;
 import event.tickets.easv.bar.be.enums.Rank;
+import event.tickets.easv.bar.bll.EmailSender;
 import event.tickets.easv.bar.bll.EntityManager;
 import event.tickets.easv.bar.gui.common.Action.CreateUser;
 import event.tickets.easv.bar.gui.common.Action.EditUser;
@@ -9,10 +11,14 @@ import event.tickets.easv.bar.gui.common.ActionHandler;
 import event.tickets.easv.bar.gui.common.UserModel;
 import event.tickets.easv.bar.gui.common.ViewHandler;
 import event.tickets.easv.bar.gui.util.BackgroundExecutor;
+import event.tickets.easv.bar.util.FailureType;
 import event.tickets.easv.bar.util.Generator;
 import event.tickets.easv.bar.util.Result;
+import event.tickets.easv.bar.util.Result.Failure;
 import event.tickets.easv.bar.util.Result.Success;
 import javafx.beans.binding.Bindings;
+
+import java.io.IOException;
 
 public class CreateUserController {
     private final CreateUserModel model;
@@ -60,11 +66,24 @@ public class CreateUserController {
 
     private Result<Boolean> resetPassword(UserModel userModel) {
         String password = Generator.generatePassword(8);
-        System.out.println("Password: " + password);
+//        System.out.println("Password: " + password);
         var tempUser = new User(userModel.id().get(), userModel.username().get(), "");
         var result = new EntityManager().resetPassword(tempUser, password);
 
         if (result.isFailure()) return result.failAs();
+
+        // todo: sender ikke mail på test
+
+/*
+        try {
+            EmailSender emailSender = new EmailSender();
+            emailSender.sendPassword(userModel.mail().get(), userModel.firstName().get(), userModel.lastName().get(), password);
+        } catch (IOException e) {
+            return Failure.of(FailureType.IO_FAILURE, "CreateUserController.createUser - failed during reading prop file for sending email", e);
+        } catch (ResendException e) {
+            return Failure.of(FailureType.EMAIL_FAILURE, "CreateUserController.createUser - failed during sending mail");
+        }
+*/
 
         return Success.of(true);
     }
