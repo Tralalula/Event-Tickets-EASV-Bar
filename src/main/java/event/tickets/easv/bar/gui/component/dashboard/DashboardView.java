145bd5f0d7ac76a67d0b498e2e705d928dd44599
Tobias <qenzoz@gmail.com>
2024-04-07 00:23:36 +0200

    Filter og sorter events på dashboard

--- src/main/java/event/tickets/easv/bar/gui/component/dashboard/DashboardView.java
+++ src/main/java/event/tickets/easv/bar/gui/component/dashboard/DashboardView.java
@@ -1,14 +1,26 @@
 package event.tickets.easv.bar.gui.component.dashboard;
 
+import atlantafx.base.theme.Styles;
 import event.tickets.easv.bar.gui.common.EventModel;
 import event.tickets.easv.bar.gui.common.NotificationType;
 import event.tickets.easv.bar.gui.common.View;
 import event.tickets.easv.bar.gui.common.ViewHandler;
 import event.tickets.easv.bar.gui.component.events.EventGridView;
+import event.tickets.easv.bar.gui.util.StyleConfig;
+import event.tickets.easv.bar.gui.widgets.Labels;
 import javafx.beans.property.BooleanProperty;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Comparator;
 
 public class DashboardView implements View {
     private final ObservableList<EventModel> eventsMasterList;
@@ -24,8 +36,21 @@ public class DashboardView implements View {
 
     @Override
     public Region getView() {
-        var gridView = new EventGridView(eventsMasterList, eventsUsersSynchronized);
+        var results = new VBox(StyleConfig.STANDARD_SPACING);
 
-        return gridView.getView();
+        var eventsTitle = Labels.styledLabel("Upcoming events", Styles.TITLE_2);
+        eventsTitle.setPadding(new Insets(0, 0, 0, 10));
+
+        var today = LocalDate.now(ZoneId.systemDefault());
+        var filteredEvents = new FilteredList<>(eventsMasterList,
+                eventModel -> !eventModel.startDate().get().isBefore(today));
+
+        var sortedEvents = new SortedList<>(filteredEvents,
+                Comparator.comparing(eventModel -> eventModel.startDate().get()));
+
+        var gridView = new EventGridView(sortedEvents, eventsUsersSynchronized);
+
+        results.getChildren().addAll(eventsTitle, gridView.getView());
+        return results;
     }
 }
