233d7b7e24c5d3ac242909e1b7989957b852cbb8
Tobias <qenzoz@gmail.com>
2024-04-07 00:41:04 +0200

    Vis en kalender på dashboard

--- src/main/java/event/tickets/easv/bar/gui/component/dashboard/DashboardView.java
+++ src/main/java/event/tickets/easv/bar/gui/component/dashboard/DashboardView.java
@@ -1,5 +1,7 @@
 package event.tickets.easv.bar.gui.component.dashboard;
 
+import atlantafx.base.controls.Calendar;
+import atlantafx.base.controls.Spacer;
 import atlantafx.base.theme.Styles;
 import event.tickets.easv.bar.gui.common.EventModel;
 import event.tickets.easv.bar.gui.common.NotificationType;
@@ -12,20 +14,24 @@ import javafx.beans.property.BooleanProperty;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
 import javafx.collections.transformation.SortedList;
-import javafx.geometry.Insets;
+import javafx.geometry.*;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.Region;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Comparator;
 
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+import static javafx.scene.layout.Region.USE_PREF_SIZE;
+
 public class DashboardView implements View {
     private final ObservableList<EventModel> eventsMasterList;
     private final BooleanProperty fetchingData;
     private final BooleanProperty eventsUsersSynchronized;
+    private static final LocalDate TODAY = LocalDate.now(ZoneId.systemDefault());
 
 
     public DashboardView(ObservableList<EventModel> eventsMasterList, BooleanProperty fetchingData, BooleanProperty eventsUsersSynchronized) {
@@ -36,14 +42,34 @@ public class DashboardView implements View {
 
     @Override
     public Region getView() {
+        var results = new GridPane();
+
+        var events = events();
+        var calender = calender();
+
+        var column1 = new ColumnConstraints();
+        column1.setHgrow(Priority.ALWAYS);
+
+        var column2 = new ColumnConstraints();
+        column2.setHgrow(Priority.NEVER);
+        column2.setPrefWidth(Region.USE_COMPUTED_SIZE);
+
+        results.getColumnConstraints().addAll(column1, column2);
+
+        results.add(events, 0, 0);
+        results.add(calender, 1, 0);
+
+        return results;
+    }
+
+    public Node events() {
         var results = new VBox(StyleConfig.STANDARD_SPACING);
 
         var eventsTitle = Labels.styledLabel("Upcoming events", Styles.TITLE_2);
         eventsTitle.setPadding(new Insets(0, 0, 0, 10));
 
-        var today = LocalDate.now(ZoneId.systemDefault());
         var filteredEvents = new FilteredList<>(eventsMasterList,
-                eventModel -> !eventModel.startDate().get().isBefore(today));
+                eventModel -> !eventModel.startDate().get().isBefore(TODAY));
 
         var sortedEvents = new SortedList<>(filteredEvents,
                 Comparator.comparing(eventModel -> eventModel.startDate().get()));
@@ -51,6 +77,18 @@ public class DashboardView implements View {
         var gridView = new EventGridView(sortedEvents, eventsUsersSynchronized);
 
         results.getChildren().addAll(eventsTitle, gridView.getView());
+        return results;
+    }
+
+    public Node calender() {
+        var results = new VBox(StyleConfig.STANDARD_SPACING);
+        results.setPadding(new Insets(0, 10, 0, 0));
+        var cal = new Calendar(TODAY);
+        cal.setShowWeekNumbers(true);
+
+        results.setAlignment(Pos.TOP_RIGHT);
+
+        results.getChildren().addAll(cal);
         return results;
     }
 }
